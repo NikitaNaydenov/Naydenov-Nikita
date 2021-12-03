4f8ab38b69e8e54dc171ee80eb4103a58a4733d0
Naydenov nikita <Graff08@mail.ru>
2021-12-03 15:30:57 +0500

    01.12.21

--- phpstorm задание/rep_index.php
+++ phpstorm задание/rep_index.php
@@ -5,32 +5,15 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
+	<link
+		href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic"
+		rel="stylesheet" />
 	<link rel="stylesheet" href="css/rep_style.css">
 
 	<title>Задание 15.11.2021</title>
 </head>
-
+<?php require_once 'inc/header.php' ?>
 <body>
-	<class="wrapper">
-		<header class="header">
-			<div class="header_container">
-				<div class="header_body">
-					<a href="index.php" class="header_logo">
-						My Header
-					</a>
-					<div class="header_main"></div>
-					<nav class="menu_body">
-						<ul class="menu_list">
-							<li class="menu_item"><a href="table_index.php" class="menu_link">Таблица Менделеева</a></li>
-							<li class="menu_item"><a href="prof_index.php" class="menu_link">Мой профайл</a></li>
-							<li class="menu_item"><a href="rep_index.php" class="menu_link">Смена фона</a></li>
-							
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</header>
 			<div class="content">
 					<?php
 						date_default_timezone_set("Asia/Yekaterinburg");
@@ -54,7 +37,7 @@
 						?>
 					</div>
 						<form action="rep_index.php" method="post">
-							<div class="block-button">
+							<div class="block_button">
 								<div class="button_morning">Утренняя тема</div>
 								<div class="button_evening">Вечерняя тема</div>
 								<div class="button_night">Ночная тема</div>
@@ -66,13 +49,6 @@
 			</div>
 		
 	</div>
-	<footer class="footer">
-		<div class="footer_block_row">
-			<div class="footer_text-main"><a href="table_index.php" class="menu_link">Таблица Менделеева</a></li></div>
-			<div class="footer_text"><a href="index.php" class="menu_link">Prod by Naydenov Nikita</a></li></div>
-			<div class="footer_text-main_2"><a href="prof_index.php" class="menu_link">Мой профайл</a></li></div>
-		</div>
-	</footer>
 </body>
-
+<?php require_once 'inc/footer.php' ?>
 </html>
