70451e95e1a5c5b91a6e6a82ac99a11834d4824e
Naydenov nikita <Graff08@mail.ru>
2021-12-20 10:00:17 +0500

    17.12.21

--- phpstorm задание/inc/footer.php
+++ phpstorm задание/inc/footer.php
@@ -1,8 +1,8 @@
 
-<footer class="footer">
+<footer class="footer"
     <div class="footer_block_row">
-        <div class="footer_text-main"><a href="table_index.php" class="menu_link">Таблица Менделеева</a></li></div>
-        <div class="footer_text"><a href="index.php" class="menu_link">Prod by Naydenov Nikita</a></li></div>
-        <div class="footer_text-main_2"><a href="prof_index.php" class="menu_link">Мой профайл</a></li></div>
+        <div class="footer_text-main"><a href="table_index.php" class="menu_link">Таблица Менделеева</a></div>
+        <div class="footer_text"><a href="index.php" class="menu_link">Prod by Naydenov Nikita</a></div>
+        <div class="footer_text-main_2"><a href="prof_index.php" class="menu_link">Мой профайл</a></div>
     </div>
 </footer>
--- phpstorm задание/index.php
+++ phpstorm задание/index.php
@@ -1,14 +1,14 @@
 <?php
-$hostname = 'localhost';
-$username = 'Nikita';
-$password = '1245';
-$dbname = 'NewCity';
+// $hostname = 'localhost';
+// $username = 'Nikita';
+// $password = '1245';
+// $dbname = 'NewCity';
 
-$dbconnect = mysqli_connect($hostname, $username, $password) or die ("-");
+//$dbconnect = mysqli_connect($hostname, $username, $password) or die ("-");
 
-mysqli_set_charset($dbconnect, 'utf8');
-mysqli_query($dbconnect, "CREATE DATABASE $dbname");
-mysqli_select_db($dbconnect, $dbname);
+// mysqli_set_charset($dbconnect, 'utf8');
+// mysqli_query($dbconnect, "CREATE DATABASE $dbname");
+// mysqli_select_db($dbconnect, $dbname);
 //mysqli_query($dbconnect, "CREATE TABLE people (ID_person int NOT NULL  AUTO_INCREMENT, Name varchar (35), SURNAME varchar (35), Age int, PRIMARY KEY (ID_person))");
 //mysqli_query($dbconnect, "INSERT INTO people (ID_person, Name, SURNAME, Age) VALUES (null, 'Mark', 'Wahlberg', 50)" );
 //mysqli_query($dbconnect, "INSERT INTO people (ID_person, Name, SURNAME, Age) VALUES (null, 'Tiger' , 'Woods', 45)" );
@@ -22,19 +22,19 @@ mysqli_select_db($dbconnect, $dbname);
 //mysqli_query($dbconnect, "INSERT INTO hobbies (ID_hobbies, Name, Description) VALUES (null, 'Хоккей', 'Игра в хоккей')" );
 
 
-mysqli_query($dbconnect, "CREATE TABLE people_hobbies (ID_record int NOT NULL AUTO_INCREMENT, ID_person INT, ID_hobbies INT, PRIMARY KEY (ID_record), FOREIGN KEY
-                            (ID_person) REFERENCES people (ID_person), FOREIGN KEY (ID_hobbies) REFERENCES hobbies (ID_hobbies))");
+// mysqli_query($dbconnect, "CREATE TABLE people_hobbies (ID_record int NOT NULL AUTO_INCREMENT, ID_person INT, ID_hobbies INT, PRIMARY KEY (ID_record), FOREIGN KEY
+//                            (ID_person) REFERENCES people (ID_person), FOREIGN KEY (ID_hobbies) REFERENCES hobbies (ID_hobbies))");
 //mysqli_query($dbconnect, "INSERT INTO people_hobbies (ID_record, ID_person, ID_hobbies)
 //VALUES (NULL, '1', '1'), (NULL, '2', '2'), (NULL, '3', '4'), (NULL, '1', '3' ) ");
 
 //mysqli_query($dbconnect, "INSERT INTO People_hobbies (ID_record, ID_person, ID_hobbies) VALUES (null, '1', '1'), (null, '2', '2'), (null, '3', '1'), (null, '2', '1')");
 
-$select= mysqli_query($dbconnect, "SELECT people.name, hobbies.name FROM people JOIN people_hobbies ON people.id_person = people_hobbies.id_person JOIN
-hobbies ON people_hobbies.id_hobbies = hobbies.id_hobbies WHERE people.name = 'Tiger'");
-$arr_select = mysqli_fetch_all($select,MYSQLI_ASSOC);
-echo '<pre>';
-var_dump($arr_select);
-echo '</pre>';
+// $select= mysqli_query($dbconnect, "SELECT people.name, hobbies.name FROM people JOIN people_hobbies ON people.id_person = people_hobbies.id_person JOIN
+// hobbies ON people_hobbies.id_hobbies = hobbies.id_hobbies WHERE people.name = 'Tiger'");
+// $arr_select = mysqli_fetch_all($select,MYSQLI_ASSOC);
+// echo '<pre>';
+// var_dump($arr_select);
+// echo '</pre>';
 
 
 
@@ -102,7 +102,7 @@ echo '</pre>';
 					<div class="flex_items">
 						<div class="block_column-one"></div>
 						<div class="block_column-figcaption">
-							<figcaption>Петергоф</figcaption>
+						<figcaption>Петергоф</figcaption>
 						</div>
 					</div>
 				</div>
@@ -110,7 +110,7 @@ echo '</pre>';
 					<div class="flex_items">
 						<div class="block_column-two"></div>
 						<div class="block_column-figcaption">
-							<figcaption>Петропавловская крепость</figcaption>
+						<figcaption>Петропавловская крепость</figcaption>
 						</div>
 					</div>
 				</div>
@@ -118,7 +118,7 @@ echo '</pre>';
 					<div class="flex_items">
 						<div class="block_column-three"></div>
 						<div class="block_column-figcaption">
-							<figcaption>Смольный</figcaption>
+						<figcaption>Смольный</figcaption>
 						</div>
 					</div>
 				</div>
@@ -126,7 +126,7 @@ echo '</pre>';
 					<div class="flex_items">
 						<div class="block_column-four"></div>
 						<div class="block_column-figcaption">
-							<figcaption>Эрмитаж</figcaption>
+						<figcaption>Эрмитаж</figcaption>
 						</div>
 					</div>
 				</div>
